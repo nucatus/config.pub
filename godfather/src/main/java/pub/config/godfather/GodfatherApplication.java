b0bedc7cab491dd50cc0818a7d67ae68c24dbb93
nucatus <alexandru.ionita@gmail.com>
2015-07-25 14:09:19 -0400

    GF-26 Properties can be retrieved as properties resource file
    A new mediaType has been added to the conntent negotiator. The type
    has to be explicitly set ar an URL parameter. The Accept header is ignored.

--- godfather/src/main/java/pub/config/godfather/GodfatherApplication.java
+++ godfather/src/main/java/pub/config/godfather/GodfatherApplication.java
@@ -2,6 +2,9 @@ package pub.config.godfather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 
 @SpringBootApplication
 public class GodfatherApplication
@@ -10,4 +13,5 @@ public class GodfatherApplication
     public static void main(String[] args) {
         SpringApplication.run(GodfatherApplication.class, args);
     }
+
 }
